6b3d4b9052f7673787163ce4893da21669e9c785
msbhamoo <mahendrabhamoo@gmail.com>
2026-08-12 22:52:48 +0530

    doc

--- application/views/setting/attendancetype.php
+++ application/views/setting/attendancetype.php
@@ -223,112 +223,64 @@ foreach ($class_value['sections'] as $section_key => $section_value) {
 												</label></div>
 											</div>
 											<?php  foreach ($list_attendance as $list_key => $list_value){ ?>
-												<div class="col-md-12 staff_attendance_row">
+													<div class="col-md-12 staff_attendance_row">
 														<div class="panel panel-info">
-														<div class="panel-footer panel-fo border-0">
-															<div class="row d-flex align-items-center justify-content-between">
-																<div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
-																		<strong>                                                                
-																			<?php echo $this->lang->line('role'); ?>: <?php  echo $list_value['role'];  ?>
-																		</strong>
-																	</div>
-																</div>
+															<div class="panel-footer panel-fo border-0">
+																<strong><?php echo $this->lang->line('role'); ?>: <?php echo $list_value['role']; ?></strong>
 															</div>
 															<div class="panel-body pr-05 ps-5">
+																<?php
+																$row = 1;
+																$prefill = array('p' => '', 'l' => '', 'f' => '', 'sh' => '');
+																$carriers = '';
+																if (!empty($attendance_type)) {
+																	foreach ($attendance_type as $att_type_value) {
+																		$rv = get_input_value($list_value['schedule'], $att_type_value->id, $att_type_value->key_value);
+																		$k = strtolower(strip_tags($att_type_value->key_value));
+																		if (array_key_exists($k, $prefill)) { $prefill[$k] = $rv['entry_time_to']; }
+																		$carriers .= '<input type="hidden" name="row[]" value="' . $row . '">';
+																		$carriers .= '<input type="hidden" name="attendance_type_id_' . $row . '" value="' . $att_type_value->id . '">';
+																		$carriers .= '<input type="hidden" name="role_id_' . $row . '" value="' . $list_value['role_id'] . '">';
+																		$carriers .= '<input type="hidden" class="hb_from hbk_' . $k . '" name="entry_time_from_' . $row . '" value="' . $rv['entry_time_from'] . '">';
+																		$carriers .= '<input type="hidden" class="hb_to hbk_' . $k . '" name="entry_time_to_' . $row . '" value="' . $rv['entry_time_to'] . '">';
+																		$carriers .= '<input type="hidden" class="hb_total hbk_' . $k . '" name="total_institute_hour_' . $row . '" value="' . $rv['total_institute_hour'] . '">';
+																		$row++;
+																	}
+																}
+																echo $carriers;
+																$dayend_prefill = ($prefill['sh'] !== '') ? $prefill['sh'] : $prefill['f'];
+																?>
 																<div class="row">
-																	<div class="col-sm-12 col-lg-12 col-md-12">
-																		<div class="col-sm-3 col-lg-3 col-md-3">
-																		    <label for="email"><?php echo $this->lang->line('attendance_type'); ?></label>
+																	<div class="col-sm-3 col-md-3">
+																		<div class="form-group">
+																			<label>Present up to (hh:mm:ss)</label>
+																			<div class="input-group"><input type="text" class="form-control time cut_present" value="<?php echo $prefill['p']; ?>" placeholder="08:15:00"><div class="input-group-addon"><span class="fa fa-clock-o"></span></div></div>
 																		</div>
-																		<div class="col-sm-9 col-lg-9 col-md-9">
-																			<div class="row">
-																				<div class="col-sm-4 col-lg-4 col-md-4">
-																					<label for="email"><?php echo $this->lang->line('entry_from'); ?> (hh:mm:ss)</label>
-																				</div>
-																				<div class="col-sm-4 col-lg-4 col-md-4">
-																					<label for="email"><?php echo $this->lang->line('entry_upto'); ?> (hh:mm:ss)</label>
-																				</div>
-																				<div class="col-sm-4 col-lg-4 col-md-4">
-																					<label for="email"><?php echo $this->lang->line('total_hour'); ?></label>
-																				</div>
-																			</div>
+																	</div>
+																	<div class="col-sm-3 col-md-3">
+																		<div class="form-group">
+																			<label>Late up to (hh:mm:ss)</label>
+																			<div class="input-group"><input type="text" class="form-control time cut_late" value="<?php echo $prefill['l']; ?>" placeholder="09:30:00"><div class="input-group-addon"><span class="fa fa-clock-o"></span></div></div>
+																		</div>
+																	</div>
+																	<div class="col-sm-3 col-md-3">
+																		<div class="form-group">
+																			<label>Half Day up to (hh:mm:ss)</label>
+																			<div class="input-group"><input type="text" class="form-control time cut_halfday" value="<?php echo $prefill['f']; ?>" placeholder="12:00:00"><div class="input-group-addon"><span class="fa fa-clock-o"></span></div></div>
+																		</div>
+																	</div>
+																	<div class="col-sm-3 col-md-3">
+																		<div class="form-group">
+																			<label>Day End Time (hh:mm:ss)</label>
+																			<div class="input-group"><input type="text" class="form-control time cut_dayend" value="<?php echo $dayend_prefill; ?>" placeholder="15:00:00"><div class="input-group-addon"><span class="fa fa-clock-o"></span></div></div>
 																		</div>
 																	</div>
 																</div>
-																
-																<div class="append_row paddA10">
-																	<?php
-																	$row = 1;
-																	if (!empty($list_value['schedule'])) {
-																		$count = 1;
-																	?>
-																		<div class="row">
-																			<div class="col-md-12">
-																				<?php
-																				if (!empty($attendance_type)) {
-																					foreach ($attendance_type as $att_type_key => $att_type_value) {
-																						$return_value = get_input_value($list_value['schedule'], $att_type_value->id, $att_type_value->key_value);
-																					
-																				?>
-																						<input type="hidden" name="row[]" value="<?php echo $row; ?>">
-																						<input type="hidden" name="attendance_type_id_<?php echo $row; ?>" value="<?php echo $att_type_value->id; ?>">
-																						<input type="hidden" name="role_id_<?php echo $row; ?>" value="<?php echo $list_value['role_id']; ?>">
-																						<div class="row">
-																							<div class="col-sm-3 col-lg-3 col-md-3">
-																								<?php  
-                                                                                                echo $this->lang->line($att_type_value->long_lang_name) . " (" . $att_type_value->key_value . ")"; ?>
-																							</div>
-																							<div class="col-sm-9 col-lg-9 col-md-9">
-																								<div class="row">
-																									<div class="col-sm-4 col-lg-4 col-md-4">
-																										<div class="form-group">
-																											
-																											<div class="input-group">
-																												<input type="text" name="entry_time_from_<?php echo $row; ?>" class="form-control entry_time_from time valid" id="entry_time_from" value="<?php echo $return_value['entry_time_from'] ?>">
-																												<div class="input-group-addon">
-																													<span class="fa fa-clock-o"></span>
-																												</div>
-																											</div>
-																										</div>
-																									</div>
-																									<div class="col-sm-4 col-lg-4 col-md-4">
-																										<div class="form-group">
-																											
-																											<div class="input-group">
-																												<input type="text" name="entry_time_to_<?php echo $row; ?>" class="form-control entry_time_to time valid" id="time_to" value="<?php echo $return_value['entry_time_to'] ?>">
-																												<div class="input-group-addon">
-																													<span class="fa fa-clock-o"></span>
-																												</div>
-																											</div>
-																										</div>
-																									</div>
-																									<div class="col-sm-4 col-lg-4 col-md-4">
-																										<div class="form-group">
-																											
-																											<div class="input-group">
-																												<input type="text" name="total_institute_hour_<?php echo $row; ?>" class="form-control total_institute_hour time_hour valid" id="total_institute_hour" value="<?php echo $return_value['total_institute_hour'] ?>">
-																												<div class="input-group-addon">
-																													<span class="fa fa-clock-o"></span>
-																												</div>
-																											</div>
-																										</div>
-																									</div>
-																								</div>
-																							</div>                                                                            
-																						</div>
-																				<?php
-																				$row++;
-																				}
-																		}
-																		?>
-																			</div>
-																		</div>
-																	<?php  $count++;   }  ?>
-																</div>
+																<p class="text-muted" style="margin:0;"><i class="fa fa-info-circle"></i> Anyone checking in up to the <strong>Present</strong> time is Present; after it up to <strong>Late</strong> is Late; after that up to <strong>Half Day</strong> is Half Day; later is Half Day (2nd half). Working hours are counted up to <strong>Day End</strong>.</p>
 															</div>
 														</div>
-												</div>
-											<?php   }      ?>
+													</div>
+																<?php   }      ?>
 										</div>
 										<div class="box-footer">
 											<button type="submit" class="btn btn-primary pull-right" data-loading-text="<i class='fa fa-spinner fa-spin '></i><?php echo $this->lang->line('update'); ?>"><?php echo $this->lang->line('save'); ?></button>
@@ -738,12 +690,59 @@ $(document).on('submit','#form_timetable',function(e){
         });
     });
 
+    // ---- Simplified staff schedule: derive the 4 attendance bands from a few
+    // single-time cut-offs (Present up to / Late up to / Half Day up to / Day
+    // End). Populates the hidden carrier inputs the backend still expects, so
+    // there is NO server-side change. Scoped to staff blocks; Student tab is
+    // untouched.
+    function _timeToSeconds(str) {
+        if (!str) return null;
+        var p = ('' + str).split(':');
+        if (p.length < 2) return null;
+        var h = parseInt(p[0], 10), m = parseInt(p[1], 10), s = parseInt(p[2] || '0', 10);
+        if (isNaN(h) || isNaN(m) || isNaN(s)) return null;
+        return h * 3600 + m * 60 + s;
+    }
+    function _secondsToHms(sec) {
+        if (sec < 0) sec = 0;
+        if (sec > 86399) sec = 86399;
+        var h = Math.floor(sec / 3600), m = Math.floor((sec % 3600) / 60), s = sec % 60;
+        function z(n){ return (n < 10 ? '0' : '') + n; }
+        return z(h) + ':' + z(m) + ':' + z(s);
+    }
+    function _setStaffBand($block, key, from, to, total) {
+        $block.find('.hb_from.hbk_' + key).val(from);
+        $block.find('.hb_to.hbk_' + key).val(to);
+        $block.find('.hb_total.hbk_' + key).val(total);
+    }
+    function genStaffBands($block) {
+        var pS = _timeToSeconds($block.find('.cut_present').val());
+        var lS = _timeToSeconds($block.find('.cut_late').val());
+        var fS = _timeToSeconds($block.find('.cut_halfday').val());
+        var dS = _timeToSeconds($block.find('.cut_dayend').val());
+        if (pS === null || lS === null || fS === null || dS === null) return; // incomplete: keep existing
+        var total = _secondsToHms(Math.max(0, dS - pS));
+        _setStaffBand($block, 'p', '00:00:00', _secondsToHms(pS), total);
+        _setStaffBand($block, 'l', _secondsToHms(pS + 1), _secondsToHms(lS), total);
+        _setStaffBand($block, 'f', _secondsToHms(lS + 1), _secondsToHms(fS), total);
+        _setStaffBand($block, 'sh', _secondsToHms(fS + 1), '23:59:59', total);
+    }
+    $(document).on('change dp.change blur', '#staff_attendance_form .cut_present, #staff_attendance_form .cut_late, #staff_attendance_form .cut_halfday, #staff_attendance_form .cut_dayend', function () {
+        var $block = $(this).closest('.staff_attendance_row');
+        if ($block.length) { genStaffBands($block); }
+    });
+    $(function () {
+        $('#staff_attendance_form .staff_attendance_row').each(function () { genStaffBands($(this)); });
+    });
+
     $(document).on('submit', '.update', function(e) {
         var submit_btn = $(this).find("button[type=submit]");
         e.preventDefault(); // avoid to execute the actual submit of the form.
 
         var form = $(this);
         var url = form.attr('action');
+        // Regenerate hidden attendance bands from the simplified cut-offs.
+        $('#staff_attendance_form .staff_attendance_row').each(function () { genStaffBands($(this)); });
 
         $.ajax({
             type: "POST",
@@ -816,14 +815,20 @@ $(document).on('submit','#form_timetable',function(e){
 
     $(document).on('change', '.copy_staff_attendance', function() {
         if (this.checked) {
-            var N = <?php echo count($attendance_type); ?>;
-            var from_times = $('#staff_attendance_form').find('.entry_time_from').slice(0, N).map(function() { return $(this).val(); }).get();
-            var to_times = $('#staff_attendance_form').find('.entry_time_to').slice(0, N).map(function() { return $(this).val(); }).get();
-            var total_hours = $('#staff_attendance_form').find('.total_institute_hour').slice(0, N).map(function() { return $(this).val(); }).get();
-
-            $('#staff_attendance_form').find('.entry_time_from').each(function(i) { $(this).val(from_times[i % N]); });
-            $('#staff_attendance_form').find('.entry_time_to').each(function(i) { $(this).val(to_times[i % N]); });
-            $('#staff_attendance_form').find('.total_institute_hour').each(function(i) { $(this).val(total_hours[i % N]); });
+            var $blocks = $('#staff_attendance_form .staff_attendance_row');
+            if (!$blocks.length) return;
+            var $first = $blocks.eq(0);
+            var p = $first.find('.cut_present').val();
+            var l = $first.find('.cut_late').val();
+            var f = $first.find('.cut_halfday').val();
+            var d = $first.find('.cut_dayend').val();
+            $blocks.each(function () {
+                $(this).find('.cut_present').val(p);
+                $(this).find('.cut_late').val(l);
+                $(this).find('.cut_halfday').val(f);
+                $(this).find('.cut_dayend').val(d);
+                genStaffBands($(this));
+            });
         }
     });
 
